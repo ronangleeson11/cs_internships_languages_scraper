6da63714cadfb25774a345903410aeb26ea3f565
Ronan <rdg1021@unh.edu>
2025-12-11 22:08:50 -0500

    Forgot to git add keywords.py earlier

---
+++ keywords.py
@@ -0,0 +1,36 @@
+languages = {
+    "Python": 0,
+    "JavaScript": 0,
+    "Java": 0,
+    "C": 0,
+    "C++": 0,
+    "C#": 0,
+    "Ruby": 0,
+    "Go": 0,
+    "PHP": 0,
+    "Swift": 0,
+    "Kotlin": 0,
+    "TypeScript": 0,
+    "SQL": 0
+}
+
+frameworks = {
+    "Django": 0,
+    "Flask": 0,
+    "React": 0,
+    "Angular": 0,
+    "Vue": 0,
+    "Spring": 0,
+    "Laravel": 0,
+    "Rails": 0,
+    "ASP.NET": 0
+}
+
+libraries = {
+    "NumPy": 0,
+    "Pandas": 0,
+    "TensorFlow": 0,
+    "PyTorch": 0,
+    "jQuery": 0,
+    "Lodash": 0
+}
